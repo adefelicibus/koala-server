4910d989cce00476e615d0cf405648149b036ce5
Alexandre Defelicibus <adefelicibus@gmail.com>
2016-02-29 17:59:36 -0300

    Fix PEP8 highlights

--- tools/gromacs-tools/definebox.py
+++ tools/gromacs-tools/definebox.py
@@ -2,114 +2,146 @@
 # -*- coding: utf-8 -*-
 
 # check_structures_gromacs.py <PDB path> <GROMACS programs path>
-# Example: python check_structures_gromacs.py /home/faccioli/Execute/1VII/ /home/faccioli/Programs/gmx-4.6.5/no_mpi/bin/
+# Example: python check_structures_gromacs.py /home/faccioli/Execute/1VII/ \
+#           /home/faccioli/Programs/gmx-4.6.5/no_mpi/bin/
 
 import sys
 import os
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE
 import shutil
 from datetime import datetime
 import getpass
 
 # obtem usuario logado no linux
 user = getpass.getuser()
+log_file_structures_not_accepted_by_definebox = 'structures_not_accepted_by_definebox.log'
+
 
 def get_gromacs_version():
-	if os.path.exists("/home/"+user+"/programs/gmx-5.0.2/"):
-		gromacs_version = "5.0.2"
-	else:
-		gromacs_version = "4.6.5"
-	return gromacs_version
+    if os.path.exists("/home/"+user+"/programs/gmx-5.0.2/"):
+        gromacs_version = "5.0.2"
+    else:
+        gromacs_version = "4.6.5"
+    return gromacs_version
 
-#Log file of structures NOT accepted by pdb2gmx
-log_file_structures_not_accepted_by_definebox='structures_not_accepted_by_definebox.log'
+
+#  Log file of structures NOT accepted by pdb2gmx
 def structure_not_accepted_by_definebox(grofile, stderr):
-	#create directory where is saved all structures that were not accepted by pdb2gmx
-	# directory = os.path.join(os.getcwd(),'no_accepted_by_pdb2gmx')
-	# if not os.path.exists(directory):
-	#	os.makedirs(directory)
-	# moves structure that was not accepted by pdb2gmx
-	# shutil.move(grofile, directory)
+    # create directory where is saved all structures that were not accepted by pdb2gmx
+    # directory = os.path.join(os.getcwd(),'no_accepted_by_pdb2gmx')
+    # if not os.path.exists(directory):
+    #   os.makedirs(directory)
+    # moves structure that was not accepted by pdb2gmx
+    # shutil.move(grofile, directory)
 
-	#write information about error at log file
-	f_log = open(log_file_structures_not_accepted_by_definebox,"a")
-	outline = "STARTING LOG OF " + grofile + "\n"
-	f_log.write(outline)
-	outline = stderr + "\n"
-	f_log.write(outline)
-	outline = "FINISHED LOG OF " + grofile + "\n\n\n"	
-	f_log.write(outline)	
-	f_log.close()
+    # write information about error at log file
+    f_log = open(log_file_structures_not_accepted_by_definebox, "a")
+    outline = "STARTING LOG OF " + grofile + "\n"
+    f_log.write(outline)
+    outline = stderr + "\n"
+    f_log.write(outline)
+    outline = "FINISHED LOG OF " + grofile + "\n\n\n"
+    f_log.write(outline)
+    f_log.close()
 
-	#write information about error at Galaxy interface
-	arq = open(log_file_structures_not_accepted_by_definebox)
-	while True:
-		line = arq.readline()
-		if not line:
-			break  # EOF
-		else:
-			sys.stderr.write(line)
-        	continue
+    # write information about error at Galaxy interface
+    arq = open(log_file_structures_not_accepted_by_definebox)
+    while True:
+        line = arq.readline()
+        if not line:
+            break  # EOF
+        else:
+            sys.stderr.write(line)
+            continue
+
 
 def definebox(group, box, distance):
-	if (get_gromacs_version() == "5.0.2"):
-		gmx_path = "/home/"+user+"/programs/gmx-5.0.2/no_mpi/bin/"	
-		program = os.path.join(gmx_path, "gmx")
-		process = Popen([program, 'editconf', '-f', 'prot.gro', '-o', 'box.gro', '-c', '-bt', box, '-d', distance, '-princ'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-	else:
-		gmx_path = "/home/"+user+"/programs/gmx-4.6.5/no_mpi/bin/"
-		program = os.path.join(gmx_path, "editconf")
-		process = Popen([program, '-f', 'prot.gro', '-o', 'box.gro', '-c', '-bt', box, '-d', distance, '-princ'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-	stdout, stderr = process.communicate(input=group)
-	#Checking output of definebox of grofile
-	if stderr.find('Fatal error') >= 0:		
-		structure_not_accepted_by_definebox('prot.gro', stderr)
-		for raiz, diretorios, arquivos in os.walk(os.getcwd()):
-			for arquivo in arquivos:
-				if not arquivo.endswith(".log") and not arquivo.endswith(".gro"):
-					os.remove(os.path.join(raiz,arquivo))
-		return 0
+    if (get_gromacs_version() == "5.0.2"):
+        gmx_path = "/home/"+user+"/programs/gmx-5.0.2/no_mpi/bin/"
+        program = os.path.join(gmx_path, "gmx")
+        process = Popen([
+            program,
+            'editconf',
+            '-f',
+            'prot.gro',
+            '-o',
+            'box.gro',
+            '-c',
+            '-bt',
+            box,
+            '-d',
+            distance,
+            '-princ'],
+            stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    else:
+        gmx_path = "/home/"+user+"/programs/gmx-4.6.5/no_mpi/bin/"
+        program = os.path.join(gmx_path, "editconf")
+        process = Popen([
+            program,
+            '-f',
+            'prot.gro',
+            '-o',
+            'box.gro',
+            '-c',
+            '-bt',
+            box,
+            '-d',
+            distance,
+            '-princ'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+    stdout, stderr = process.communicate(input=group)
+
+    # Checking output of definebox of grofile
+    if stderr.find('Fatal error') >= 0:
+        structure_not_accepted_by_definebox('prot.gro', stderr)
+        for raiz, diretorios, arquivos in os.walk(os.getcwd()):
+            for arquivo in arquivos:
+                if not arquivo.endswith(".log") and not arquivo.endswith(".gro"):
+                    os.remove(os.path.join(raiz, arquivo))
+        return 0
+
 
 def main():
-	#Avoid GROMACS backup files
-	os.environ["GMX_MAXBACKUP"]="-1"
 
-	# define e acessa diretório padrão de execução
-	diretorio = "/home/"+user+"/execute/"
-	os.chdir(diretorio)
+    # Avoid GROMACS backup files
+    os.environ["GMX_MAXBACKUP"] = "-1"
 
-	# cria e acessa diretório temporário nomeado pela data completa atual sem espaços
-	date = datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace(' ', '_')
-	os.makedirs(date)
-	os.chdir(date)
+    # define e acessa diretório padrão de execução
+    diretorio = "/home/"+user+"/execute/"
+    os.chdir(diretorio)
 
-	# copia parâmetro de entrada para a nova pasta temporária com o nome correto
-	shutil.copy(sys.argv[1], "prot.gro")
+    # cria e acessa diretório temporário nomeado pela data completa atual sem espaços
+    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace(' ', '_')
+    os.makedirs(date)
+    os.chdir(date)
 
-	# define inputs
-	group = sys.argv[2]
-	box = sys.argv[3]
-	distance = sys.argv[4]
+    # copia parâmetro de entrada para a nova pasta temporária com o nome correto
+    shutil.copy(sys.argv[1], "prot.gro")
 
-	# roda a funcao
-	result = definebox(group, box, distance)
+    # define inputs
+    group = sys.argv[2]
+    box = sys.argv[3]
+    distance = sys.argv[4]
 
-	# definebox ok
-	if (result != 0):
+    # roda a funcao
+    result = definebox(group, box, distance)
 
-		# arquivos de saída
-		gro = "box.gro"
+    # definebox ok
+    if (result != 0):
 
-		# cópia dos arquivos de saída para o dataset
-		shutil.copy(gro, sys.argv[5])
+        # arquivos de saída
+        gro = "box.gro"
 
-		# remove arquivos do diretório temporário
-		for raiz, diretorios, arquivos in os.walk(os.getcwd()):
-			for arquivo in arquivos:
-				os.remove(os.path.join(raiz,arquivo))
+        # cópia dos arquivos de saída para o dataset
+        shutil.copy(gro, sys.argv[5])
 
-		# remove diretório temporário
-		os.chdir(diretorio)
-		os.rmdir(date)
+        # remove arquivos do diretório temporário
+        for raiz, diretorios, arquivos in os.walk(os.getcwd()):
+            for arquivo in arquivos:
+                os.remove(os.path.join(raiz, arquivo))
+
+        # remove diretório temporário
+        os.chdir(diretorio)
+        os.rmdir(date)
 
 main()
--- tools/gromacs-tools/solvate.py
+++ tools/gromacs-tools/solvate.py
@@ -2,112 +2,139 @@
 # -*- coding: utf-8 -*-
 
 # check_structures_gromacs.py <PDB path> <GROMACS programs path>
-# Example: python check_structures_gromacs.py /home/faccioli/Execute/1VII/ /home/faccioli/Programs/gmx-4.6.5/no_mpi/bin/
+# Example: python check_structures_gromacs.py /home/faccioli/Execute/1VII/ \
+#          /home/faccioli/Programs/gmx-4.6.5/no_mpi/bin/
 
 import sys
 import os
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE
 import shutil
 from datetime import datetime
 import getpass
 
 # obtem usuario logado no linux
 user = getpass.getuser()
+log_file_structures_not_accepted_by_solvate = 'structures_not_accepted_by_solvate.log'
+
 
 def get_gromacs_version():
-	if os.path.exists("/home/"+user+"/programs/gmx-5.0.2/"):
-		gromacs_version = "5.0.2"
-	else:
-		gromacs_version = "4.6.5"
-	return gromacs_version
+    if os.path.exists("/home/"+user+"/programs/gmx-5.0.2/"):
+        gromacs_version = "5.0.2"
+    else:
+        gromacs_version = "4.6.5"
+    return gromacs_version
 
-#Log file of structures NOT accepted by pdb2gmx
-log_file_structures_not_accepted_by_solvate='structures_not_accepted_by_solvate.log'
+
+# Log file of structures NOT accepted by pdb2gmx
 def structure_not_accepted_by_definebox(grofile, stderr):
-	#create directory where is saved all structures that were not accepted by pdb2gmx
-	# directory = os.path.join(os.getcwd(),'no_accepted_by_pdb2gmx')
-	# if not os.path.exists(directory):
-	#	os.makedirs(directory)
-	# moves structure that was not accepted by pdb2gmx
-	# shutil.move(grofile, directory)
+    # create directory where is saved all structures that were not accepted by pdb2gmx
+    # directory = os.path.join(os.getcwd(),'no_accepted_by_pdb2gmx')
+    # if not os.path.exists(directory):
+    #   os.makedirs(directory)
+    # moves structure that was not accepted by pdb2gmx
+    # shutil.move(grofile, directory)
 
-	#write information about error at log file
-	f_log = open(log_file_structures_not_accepted_by_solvate,"a")
-	outline = "STARTING LOG OF " + grofile + "\n"
-	f_log.write(outline)
-	outline = stderr + "\n"
-	f_log.write(outline)
-	outline = "FINISHED LOG OF " + grofile + "\n\n\n"	
-	f_log.write(outline)	
-	f_log.close()
+    # write information about error at log file
+    f_log = open(log_file_structures_not_accepted_by_solvate, "a")
+    outline = "STARTING LOG OF " + grofile + "\n"
+    f_log.write(outline)
+    outline = stderr + "\n"
+    f_log.write(outline)
+    outline = "FINISHED LOG OF " + grofile + "\n\n\n"
+    f_log.write(outline)
+    f_log.close()
 
-	#write information about error at Galaxy interface
-	arq = open(log_file_structures_not_accepted_by_solvate)
-	while True:
-		line = arq.readline()
-		if not line:
-			break  # EOF
-		else:
-			sys.stderr.write(line)
-        	continue
+    # write information about error at Galaxy interface
+    arq = open(log_file_structures_not_accepted_by_solvate)
+    while True:
+        line = arq.readline()
+        if not line:
+            break  # EOF
+        else:
+            sys.stderr.write(line)
+            continue
+
 
 def solvate():
-	if (get_gromacs_version() == "5.0.2"):
-		gmx_path = "/home/"+user+"/programs/gmx-5.0.2/no_mpi/bin/"
-		program = os.path.join(gmx_path, "gmx")
-		process = Popen([program, 'solvate', '-o', 'water', '-cp', 'box.gro', '-p', 'top.top', '-cs', 'spc216.gro'], stdout=PIPE, stderr=PIPE)
-	else:
-		gmx_path = "/home/"+user+"/programs/gmx-4.6.5/no_mpi/bin/"
-		program = os.path.join(gmx_path, "genbox")
-		process = Popen([program, '-o', 'water', '-cp', 'box.gro', '-p', 'top.top', '-cs', 'spc216.gro'], stdout=PIPE, stderr=PIPE)
-	stdout, stderr = process.communicate()
-	#Checking output of solvate of grofile
-	if stderr.find('Fatal error') >= 0:		
-		structure_not_accepted_by_solvate('box.gro', stderr)
-		for raiz, diretorios, arquivos in os.walk(os.getcwd()):
-			for arquivo in arquivos:
-				if not arquivo.endswith(".log") and not arquivo.endswith(".gro"):
-					os.remove(os.path.join(raiz,arquivo))
-		return 0
+    if (get_gromacs_version() == "5.0.2"):
+        gmx_path = "/home/"+user+"/programs/gmx-5.0.2/no_mpi/bin/"
+        program = os.path.join(gmx_path, "gmx")
+        process = Popen([
+            program,
+            'solvate',
+            '-o',
+            'water',
+            '-cp',
+            'box.gro',
+            '-p',
+            'top.top',
+            '-cs',
+            'spc216.gro'],
+            stdout=PIPE, stderr=PIPE)
+    else:
+        gmx_path = "/home/"+user+"/programs/gmx-4.6.5/no_mpi/bin/"
+        program = os.path.join(gmx_path, "genbox")
+        process = Popen([
+            program,
+            '-o',
+            'water',
+            '-cp',
+            'box.gro',
+            '-p',
+            'top.top',
+            '-cs',
+            'spc216.gro'],
+            stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+
+    # Checking output of solvate of grofile
+    if stderr.find('Fatal error') >= 0:
+        structure_not_accepted_by_definebox('box.gro', stderr)
+        for raiz, diretorios, arquivos in os.walk(os.getcwd()):
+            for arquivo in arquivos:
+                if not arquivo.endswith(".log") and not arquivo.endswith(".gro"):
+                    os.remove(os.path.join(raiz, arquivo))
+        return 0
+
 
 def main():
-	#Avoid GROMACS backup files
-	os.environ["GMX_MAXBACKUP"]="-1"
+    # Avoid GROMACS backup files
+    os.environ["GMX_MAXBACKUP"] = "-1"
 
-	# define e acessa diretório padrão de execução
-	diretorio = "/home/"+user+"/execute/"
-	os.chdir(diretorio)
+    # define e acessa diretório padrão de execução
+    diretorio = "/home/"+user+"/execute/"
+    os.chdir(diretorio)
 
-	# cria e acessa diretório temporário nomeado pela data completa atual sem espaços
-	date = datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace(' ', '_')
-	os.makedirs(date)
-	os.chdir(date)
+    # cria e acessa diretório temporário nomeado pela data completa atual sem espaços
+    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace(' ', '_')
+    os.makedirs(date)
+    os.chdir(date)
 
-	# copia parâmetro de entrada para a nova pasta temporária com o nome correto
-	shutil.copy(sys.argv[1], "top.top")
-	shutil.copy(sys.argv[2], "box.gro")
+    # copia parâmetro de entrada para a nova pasta temporária com o nome correto
+    shutil.copy(sys.argv[1], "top.top")
+    shutil.copy(sys.argv[2], "box.gro")
 
-	# roda a funcao
-	result = solvate()
+    # roda a funcao
+    result = solvate()
 
-	# solvate ok
-	if (result != 0):
+    # solvate ok
+    if (result != 0):
 
-		# arquivos de saída
-		gro = "water.gro"
-		top = "top.top"
+        # arquivos de saída
+        gro = "water.gro"
+        top = "top.top"
 
-		# cópia dos arquivos de saída para o dataset
-		shutil.copy(gro, sys.argv[3])
-		shutil.copy(top, sys.argv[4])
+        # cópia dos arquivos de saída para o dataset
+        shutil.copy(gro, sys.argv[3])
+        shutil.copy(top, sys.argv[4])
 
-		# remove arquivos do diretório temporário
-		for raiz, diretorios, arquivos in os.walk(os.getcwd()):
-			for arquivo in arquivos:
-				os.remove(os.path.join(raiz,arquivo))
+        # remove arquivos do diretório temporário
+        for raiz, diretorios, arquivos in os.walk(os.getcwd()):
+            for arquivo in arquivos:
+                os.remove(os.path.join(raiz, arquivo))
 
-		# remove diretório temporário
-		os.chdir(diretorio)
-		os.rmdir(date)
+        # remove diretório temporário
+        os.chdir(diretorio)
+        os.rmdir(date)
 
 main()
